10a843df3b5f3236496e4836f93a5033de31a20b
davdane <davide.danese@student.univaq.it>
2020-05-28 15:49:29 +0200

    Login and Register + routing for add profile

--- Project/src/app/app-routing.module.ts
+++ Project/src/app/app-routing.module.ts
@@ -10,13 +10,22 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },  {
+  },
+  {
     path: 'add-profile',
     loadChildren: () => import('./add-profile/add-profile.module').then( m => m.AddProfilePageModule)
   },
   {
     path: 'add-appoint',
     loadChildren: () => import('./add-appoint/add-appoint.module').then( m => m.AddAppointPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
 
 ];
