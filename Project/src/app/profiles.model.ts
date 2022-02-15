dbf646315a394e322898b011237ee14da2626e92
davdane <62146015+davdane@users.noreply.github.com>
2022-02-15 19:14:43 +0100

    Delete Project directory

--- Project/src/app/profiles.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Profiles {
-    id_profiles?: string;
-    id_user?: string;
-    nome?: string;
-    cognome?: string;
-    altezza?: string;
-    peso?: string;
-    data?: string;
-  }
